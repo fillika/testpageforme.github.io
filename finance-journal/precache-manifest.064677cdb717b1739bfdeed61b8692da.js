2ac8f9c96c0ed570a47d87170d32c4e337073388
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-01-23 21:08:33 +0300

    Первый деплой

---
+++ finance-journal/precache-manifest.064677cdb717b1739bfdeed61b8692da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e458359910e04215f8f00a2f23189c86",
+    "url": "/testpageforme.github.io/finance-journal/index.html"
+  },
+  {
+    "revision": "1ff18d77a1eee7ad71c8",
+    "url": "/testpageforme.github.io/finance-journal/static/css/main.04c82a06.chunk.css"
+  },
+  {
+    "revision": "1dca83af33961b83ce42",
+    "url": "/testpageforme.github.io/finance-journal/static/js/2.3ba15fb1.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/testpageforme.github.io/finance-journal/static/js/2.3ba15fb1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ff18d77a1eee7ad71c8",
+    "url": "/testpageforme.github.io/finance-journal/static/js/main.0e9f50b7.chunk.js"
+  },
+  {
+    "revision": "b927fc402f058bc15c19",
+    "url": "/testpageforme.github.io/finance-journal/static/js/runtime-main.d2fdf836.js"
+  }
+]);
