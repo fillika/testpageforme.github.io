70026b1c0aae8ebfd4cfff18dfa345ac03c5fdcb
fillika <49833052+fillika@users.noreply.github.com>
2020-02-05 11:24:28 +0300

    2

--- finance-journal/service-worker.js
+++ finance-journal/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1708cf40c224a20a7edbaf723c52df93.js"
+  "./precache-manifest.e5321463c89adaffa2d52e478041b00f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
